064a21772a4aa49e9ab59873c648541d9d8f78b5
mbikandi <mbikandi@bcamath.org>
2024-07-19 12:17:14 +0200

    finetune with additional_loss

---
+++ finetune_shift_additional_loss.py
@@ -0,0 +1,33 @@
+'''
+Script to pretrain on the SHIFT dataset for 100 epochs
+'''
+
+import torch
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, TQDMProgressBar
+
+from utils.helper_functions import ShiftSegmentationDataModule
+from data.shift_dataset import LabelFilter
+from nets.wrapper import Wrapper
+
+dm = ShiftSegmentationDataModule(
+    "./datasets/SHIFT", 512, 16, LabelFilter("4", -1, 0), LabelFilter("4", -1, 0), "ood_pedestrian", 8, .05)
+
+model = Wrapper("resnet50", 21, .0001)
+model.load_state_dict(torch.load("./pretrained/sh_weights.ckpt"))
+model.additional_loss = True
+
+ckpt = ModelCheckpoint(
+        monitor="val_miou",
+        mode="max",
+        save_top_k=2,
+        save_last=True,
+        filename='{epoch}-{step}-{val_miou:.4f}'
+    )
+
+tr = Trainer(default_root_dir="./test_shift_additional_loss", accelerator="cuda",
+        callbacks=[ckpt, LearningRateMonitor(logging_interval="epoch"),
+        TQDMProgressBar(refresh_rate=1)], max_epochs=10)
+
+tr.fit(model=model, datamodule=dm)
